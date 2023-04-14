36e51e76f5a7484fdaafc6c0d3f5ac993d846280
akunzler <andrin.kuenzler@stud.hslu.ch>
2023-04-14 09:37:15 +0200

    Change launcher.sh to root start

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # launcher.sh
 # navigate to home directory, then to this directory, then execute python script, then back home
-cd /var/www/html/nodejs-server && (npm run start&)
-cd /var/www/html/frontend && (npm run start&)
+cd /var/www/html/nodejs-server && (sudo npm run start&)
+cd /var/www/html/frontend && (sudo npm run start&)
